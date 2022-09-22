02ff66d83ec8977d91b44e23cdb2118d3d1c8dc8
Ansh <99464146+AnshMinchekar@users.noreply.github.com>
2022-09-22 12:21:51 +0530

    Create day3.py
    Learning concatination.

---
+++ day3.py
@@ -0,0 +1,5 @@
+//Concatination
+Movie = input("Name of the movie: ")
+Year = input("Release year of the movie: ")
+Rating = input("How would you rate it out of 5? :")
+print("You watched", Movie, "released in", Year, "and rated it", Rating)
