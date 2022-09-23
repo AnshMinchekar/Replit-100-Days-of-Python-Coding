3fed85de67828d0cb1627b1bb7509a406c578b0b
Ansh <99464146+AnshMinchekar@users.noreply.github.com>
2022-09-24 01:27:02 +0530

    Create Day5.py
    Learnt If-else statements on Day 5 of Replit's 100 days of Python coding.

---
+++ Day5.py
@@ -0,0 +1,15 @@
+Spiderman = input("""Are you "Amazing"?" """)
+if Spiderman == "Yes":
+    print("Then you're Spider-Man")
+else:
+    print("I guess you're not Amazing or Spider-Man.")
+IronMan = input("""Are you "Invincible"?" """)
+if IronMan == "Yes":
+    print("Then you are Iron Man")
+else:
+    print("I guess you're not Invincible or Iron Man.")
+Thor = input("""Are you "Mighty"?" """)
+if Thor == "Yes":
+    print("Then you are Thor")
+else:
+    print("I guess you're not Mighty or Thor.")
