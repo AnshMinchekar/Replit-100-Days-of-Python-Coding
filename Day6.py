d627566ae41b83bfc133df1313a94b64858045ea
Ansh <99464146+AnshMinchekar@users.noreply.github.com>
2022-09-25 00:19:15 +0530

    Create Day6.py
    Learned elif at Replit's 100 days of Python coding.

---
+++ Day6.py
@@ -0,0 +1,12 @@
+print("Welcome to the Login page!")
+Name = input("USERNAME> ")
+Password = input("PASSWORD> ")
+print("................")
+if Name == "Ansh" and Password == "L23B16":
+  print("Hello there Ansh!")
+elif Name == "Aditya" and Password == "Princeton":
+  print("Welcome Aditya!")
+elif Name == "Tanzila" and Password == "Mumbai":
+  print("Good day Tanzila!")
+else: 
+ print("Who are you again?")
